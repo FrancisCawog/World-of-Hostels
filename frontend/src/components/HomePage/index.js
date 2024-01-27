baaec48a574b693e20ec07f854413e5d9f494803
Francis Cawog <Franciscawog@gmail.com>
2024-01-26 20:51:35 -0500

    added to splash page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useInView } from 'react-intersection-observer';
 import './Homepage.css';
 import groupPhoto from "../../assets/pictures/hero.chats.webp"
 import arrow from "../../assets/pictures/icons/arrow.webp"
@@ -9,9 +10,35 @@ import walking from "../../assets/pictures/walkingusp.webp"
 import Navigation from "../Navigation";
 import Footer from "../Footer";
 import SearchBar from "../SearchBar";
+import car from "../../assets/pictures/The_Spindrift_Hostel-img.jpg"
+import MyArrowSVG from "../../assets/pictures/icons/arrow-left.svg"
+import hostel from "../../assets/pictures/los-patios-card_png.jpg"
+import user1 from "../../assets/profile-pics/user1.jpeg"
+import user2 from "../../assets/profile-pics/user2.jpeg"
+import user3 from "../../assets/profile-pics/user3.jpeg"
+import user4 from "../../assets/profile-pics/user4.jpeg"
+import user5 from "../../assets/profile-pics/user5.jpeg"
+import user6 from "../../assets/profile-pics/user6.jpeg"
+import user7 from "../../assets/profile-pics/user7.jpeg"
+import user8 from "../../assets/profile-pics/user8.jpeg"
+import user9 from "../../assets/profile-pics/user9.jpeg"
+import user10 from "../../assets/profile-pics/user10.jpeg"
+import user11 from "../../assets/profile-pics/user11.jpeg"
 
 export default function HomePage() {
   sessionStorage.setItem('redirectUrl', window.location.pathname);
+
+  const handleClick = (e) => {
+    const checkNew = document.getElementById('see-who-going');
+      if (checkNew) {
+        checkNew.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+  }  
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
 
   return (
     <>
@@ -38,7 +65,6 @@ export default function HomePage() {
         <span className="bold">Free Cancellation</span> & <span className="bold">Flexible Booking</span> available
       </p>
 
-        <br/>
         <br/>
         <br/>
         <div className="boxContainer">
@@ -73,7 +99,82 @@ export default function HomePage() {
           </div>
         </div>
     </div>
-    <br/>
+    <div className="this-is">
+      <div className="this-is-text">
+        <div className="this-text">
+          <p>
+            This is the
+            <span style={{color: "#cc0074", marginLeft: "3px", marginRight: "3px"}}>NEW</span> 
+            Hostelworld.
+          </p>
+          <h2>
+            Helping you
+            <span style={{color: "#cc0074", marginLeft: "10px"}}>connect with travellers</span>
+            . Even
+            <span style={{color: "#cc0074", marginLeft: "10px", marginRight: "10px"}}>before</span>
+            you get to your hostel
+          </h2>
+          <button onClick={handleClick}>Check what's new
+            <img className="arrow-down-this" src={MyArrowSVG}/>
+          </button>
+        </div>
+      </div>
+      <div className="this-is-car">
+        <img src={car}/>
+      </div>
+    </div>
+
+    <div className="see-who-going" id="see-who-going" ref={ref}>
+    {inView &&
+      <div className="animation" >
+        <img src={hostel} />
+        <div>
+          <div className="user-1" >
+            <img src={user1} />
+          </div>
+          <div className="user-2">
+            <img src={user2} />
+          </div>
+          <div className="user-3">
+            <img src={user3} />
+          </div>
+          <div className="user-4">
+            <img src={user4} />
+          </div>
+          <div className="user-5">
+            <img src={user5} />
+          </div>
+          <div className="user-6">
+           <img src={user6} />
+          </div>
+          <div className="user-7">
+           <img src={user7} />
+          </div>
+          <div className="user-8">
+           <img src={user8} />
+          </div>
+          <div className="user-9">
+            <img src={user9} />
+          </div>
+          <div className="user-10">
+            <img src={user10} />
+          </div>
+          <div className="user-11">
+           <img src={user11} />
+          </div>
+        </div>
+      </div>
+      }
+      <div className="see-who-going-text">
+          <h2>
+            See
+            <span style={{marginLeft: "15px", marginRight: "15px", color: "orange"}} >who's</span>
+            going.
+          </h2>
+          <p>Connect with other travellers staying in the same hostel or city as you.</p>
+      </div>
+    </div>
+
     <Footer/>
     </>
   );
