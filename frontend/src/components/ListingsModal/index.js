64a21ed7445bfd8d0523aa66e3f2d29fe48b9c9f
Francis Cawog <Franciscawog@gmail.com>
2024-06-12 18:17:04 -0400

    memoized listings modal

--- frontend/src/components/ListingsModal/index.js
+++ frontend/src/components/ListingsModal/index.js
@@ -1,3 +1,5 @@
+//Memoized
+
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchListing } from '../../store/listings';
@@ -11,8 +13,8 @@ import FacilityIcon from "../../components/FacilityIcon/index.js";
 
 const ListingsModal = ({ tabName, onClose }) => {
   const cart = useSelector((state) => state.cart);
-  const checkIn = cart.checkIn
-  const checkOut = cart.checkOut
+  const checkIn = cart.checkIn;
+  const checkOut = cart.checkOut;
   const start_date = new Date(checkIn).toISOString();
   const end_date = new Date(checkOut).toISOString();
   const dispatch = useDispatch();
@@ -39,14 +41,15 @@ const ListingsModal = ({ tabName, onClose }) => {
 
   useEffect(() => {
     if (checkIn !== null) {
-    dispatch(fetchListing(listingId, start_date, end_date)).catch((error) => {
-      console.error('Error fetching listing:', error);
-    })
+      dispatch(fetchListing(listingId, start_date, end_date)).catch((error) => {
+        console.error('Error fetching listing:', error);
+      });
     } else {
       dispatch(fetchListing(listingId, today.toISOString().split("T")[0], tomorrow.toISOString().split("T")[0])).catch((error) => {
         console.error('Error fetching listing:', error);
-      })}
-    }, [listingId, dispatch]);
+      });
+    }
+  }, [listingId, checkIn, checkOut, dispatch]);
 
   const handleTabClick = (tabName) => {
     setActiveTab(tabName);
@@ -72,157 +75,148 @@ const ListingsModal = ({ tabName, onClose }) => {
     scaledSize: markerImageSize,
   };
 
-    return (
-        <div className="modal-overlay" onClick={onClose}>
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
-            <div className="modal-content">
-            <span className="close" onClick={onClose}>&times;</span>
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-content">
+          <span className="close" onClick={onClose}>&times;</span>
 
-            {tabName === 'Map' ? (
-                <>
-                <h2 className='modal-header'>Location</h2>
-                <GoogleMap
-                  mapContainerStyle={mapStyles}
-                  zoom={18}
-                  center={{
+          {tabName === 'Map' ? (
+            <>
+              <h2 className='modal-header'>Location</h2>
+              <GoogleMap
+                mapContainerStyle={mapStyles}
+                zoom={18}
+                center={{
+                  lat: listing.latitude,
+                  lng: listing.longitude,
+                }}
+              >
+                <Marker
+                  position={{
                     lat: listing.latitude,
                     lng: listing.longitude,
                   }}
-                >
-                  <Marker
-                    position={{
-                      lat: listing.latitude,
-                      lng: listing.longitude,
-                    }}
-                    onClick={handleMarkerClick}
-                    icon={customMarkerIcon}
-                  />
-                  {infoWindowVisible && infoWindowPosition && (
-                    <InfoWindow
-                      position={infoWindowPosition}
-                      onCloseClick={() => setInfoWindowVisible(false)}
-                    >
-                      <div>
-                        <h3 style={{ fontFamily: "Inter", fontSize: "20px" }}>{listing.property_name}</h3>
-                        <p style={{ fontFamily: "Inter", fontWeight: "400", padding: "0px" }}>{listing.address}</p>
-                        <p style={{ fontFamily: "Inter", fontWeight: "400", marginTop: "-10px" }}>{`${listing.city}, ${listing.country}`}</p>
-                      </div>
-                    </InfoWindow>
-                  )}
-                </GoogleMap>
-              </>
-                ) : (
+                  onClick={handleMarkerClick}
+                  icon={customMarkerIcon}
+                />
+                {infoWindowVisible && infoWindowPosition && (
+                  <InfoWindow
+                    position={infoWindowPosition}
+                    onCloseClick={() => setInfoWindowVisible(false)}
+                  >
+                    <div>
+                      <h3 style={{ fontFamily: "Inter", fontSize: "20px" }}>{listing.property_name}</h3>
+                      <p style={{ fontFamily: "Inter", fontWeight: "400", padding: "0px" }}>{listing.address}</p>
+                      <p style={{ fontFamily: "Inter", fontWeight: "400", marginTop: "-10px" }}>{`${listing.city}, ${listing.country}`}</p>
+                    </div>
+                  </InfoWindow>
+                )}
+              </GoogleMap>
+            </>
+          ) : (
             <>
-            <h2 className='modal-header'>Property details</h2>
-            <div className="horizontal-line"></div>
-            <div className="tabs-container">
-                
+              <h2 className='modal-header'>Property details</h2>
+              <div className="horizontal-line"></div>
+              <div className="tabs-container">
                 <div className="modal-tabs">
-                    <div
-                        className={`tab ${activeTab === 'About' ? 'active' : ''}`}
-                        onClick={() => handleTabClick('About')}
-                    >
-                        About
-                    </div>
-                    <div
-                        className={`tab ${activeTab === 'Facilities' ? 'active' : ''}`}
-                        onClick={() => handleTabClick('Facilities')}
-                    >
-                        Facilities
-                    </div>
-                    <div
-                        className={`tab ${activeTab === 'HouseRules' ? 'active' : ''}`}
-                        onClick={() => handleTabClick('HouseRules')}
-                    >
-                        House Rules
-                    
-                    </div>
-                </div>
-            </div>
-            <div className="tab-underline"></div>
-
-            {activeTab === 'About' && (  
-                <div className="tab-content">
-                  <p> {listing.description}</p> 
-                </div> 
-            )}
-
-        {activeTab === 'Facilities' && (
-          <div className="tab-content">
-            {allKeys.map((key, index) => (
-              <>
-              <div key={key}>
-                <p className='facilities-key'>{key}</p>
-                <div className='facilities-modal-list'>
-                {facilitiesObject[key].map((item) => (
-                  <>
-                  <div className='facilities-modal-list-item'>
-                    <div className="facilities-icon2">
-                      <FacilityIcon item={item} />
-                    </div>
-                    {item}
+                  <div
+                    className={`tab ${activeTab === 'About' ? 'active' : ''}`}
+                    onClick={() => handleTabClick('About')}
+                  >
+                    About
                   </div>
-                  </>
-                ))}
+                  <div
+                    className={`tab ${activeTab === 'Facilities' ? 'active' : ''}`}
+                    onClick={() => handleTabClick('Facilities')}
+                  >
+                    Facilities
+                  </div>
+                  <div
+                    className={`tab ${activeTab === 'HouseRules' ? 'active' : ''}`}
+                    onClick={() => handleTabClick('HouseRules')}
+                  >
+                    House Rules
+                  </div>
                 </div>
               </div>
-              {index < allKeys.length - 1 && (
-            <div className="horizontal-line" id="facilities-modal-line" />
-          )}
-              </>
-            ))}
-          </div>
-        )}
+              <div className="tab-underline"></div>
 
-            {activeTab === 'HouseRules' && ( 
-                <>
-                <h2 style={{fontFamily: "Poppins-bold", fontSize: "20px", marginLeft: "2%", marginTop: "-.5%"}}>Hostel Policies</h2>
-                    <div id='modal-check' className="checkInandOut">
-                        <div className="checkInContainer">
-                        <img className="checkIn" src={checkInPic} style={{ width: '18px' }}/>
-                        <div className="checkInText">
-                            Check In
-                            <div className="check_in"> {listing?.check_in}
-                            </div>
-                        </div>
-                        </div>
-                        <div className="separator"></div>
-                        <div className="checkOutContainer">
-                            <img className="checkOut" src={checkOutPic} style={{ width: '18px' }}/>
-                            <div className="checkOutText">
-                                Check Out
-                                <div className="check_out"> until {listing?.check_out}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="smaller-horizontal-line"> </div>
-
+              {activeTab === 'About' && (
                 <div className="tab-content">
-                <h2 style={{fontFamily: "Poppins-bold", fontSize: "20px", marginTop: "15px"}}>Cancellation Policy</h2>
-                <div className='cancellation-policy'>
-                    <div className='free-cancellation-rates'>
+                  <p>{listing.description}</p>
+                </div>
+              )}
+
+              {activeTab === 'Facilities' && (
+                <div className="tab-content">
+                  {allKeys.map((key, index) => (
+                    <div key={key}>
+                      <p className='facilities-key'>{key}</p>
+                      <div className='facilities-modal-list'>
+                        {facilitiesObject[key].map((item, i) => (
+                          <div key={i} className='facilities-modal-list-item'>
+                            <div className="facilities-icon2">
+                              <FacilityIcon item={item} />
+                            </div>
+                            {item}
+                          </div>
+                        ))}
+                      </div>
+                      {index < allKeys.length - 1 && (
+                        <div className="horizontal-line" id="facilities-modal-line" />
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+
+              {activeTab === 'HouseRules' && (
+                <>
+                  <h2 style={{ fontFamily: "Poppins-bold", fontSize: "20px", marginLeft: "2%", marginTop: "-.5%" }}>Hostel Policies</h2>
+                  <div id='modal-check' className="checkInandOut">
+                    <div className="checkInContainer">
+                      <img className="checkIn" src={checkInPic} style={{ width: '18px' }} alt="Check In" />
+                      <div className="checkInText">
+                        Check In
+                        <div className="check_in">{listing?.check_in}</div>
+                      </div>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="checkOutContainer">
+                      <img className="checkOut" src={checkOutPic} style={{ width: '18px' }} alt="Check Out" />
+                      <div className="checkOutText">
+                        Check Out
+                        <div className="check_out">until {listing?.check_out}</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="smaller-horizontal-line"></div>
+
+                  <div className="tab-content">
+                    <h2 style={{ fontFamily: "Poppins-bold", fontSize: "20px", marginTop: "15px" }}>Cancellation Policy</h2>
+                    <div className='cancellation-policy'>
+                      <div className='free-cancellation-rates'>
                         <h2>Free Cancellation rates</h2>
                         <p>If you cancel your booking before 23:59 7 days before your check-in date your deposit will be refunded to your payment card. If you cancel after 23:59 7 days or in the event of a no-show, your deposit will not be refunded and the total of the first night's accommodation will be charged to your payment card, unless otherwise stated (see 'Things to Note' information). Please note that cancellations must be made directly within your booking in My Account.</p>
-                    </div>
-                    <div className='non-refundable-rates'>
+                      </div>
+                      <div className='non-refundable-rates'>
                         <h2>Non-refundable rates</h2>
                         <p>Your booking is non-refundable. If you choose to cancel your booking, or in the case of a no-show, you will still be charged the full cost of your booking</p>
+                      </div>
                     </div>
-                </div>
-                <div className="smaller-horizontal-line" style={{marginLeft: "1px", marginRight: "1px", marginBottom: "-15px"}}> </div>
-                <h2 style={{fontFamily: "Poppins-bold", fontSize: "20px", marginTop: "15px", marginBottom: "-25px"}}>Things to Note</h2>
-                <p> {listing?.house_rules}</p> 
-                </div> 
+                    <div className="smaller-horizontal-line"></div>
+                    <h2 style={{ fontFamily: "Poppins-bold", fontSize: "20px", marginTop: "15px", marginBottom: "-25px" }}>Things to Note</h2>
+                    <p>{listing?.house_rules}</p>
+                  </div>
                 </>
-                
-                )}
-                </>
-            )}
-            </div>
+              )}
+            </>
+          )}
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ListingsModal;
--- frontend/src/components/ListingsShowReviewModal/index.js
+++ frontend/src/components/ListingsShowReviewModal/index.js
@@ -1,3 +1,5 @@
+//Memoized
+
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
