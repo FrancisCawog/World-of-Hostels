cfb02cbedaa14993ce6eaaf9adb004fb65136f72
Francis Cawog <Franciscawog@gmail.com>
2024-06-27 14:08:25 -0400

    refactored home page worlds best

---
+++ frontend/src/components/HomePage/WorldsBest/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import "./WorldsBest.css"
+
+export default function WorldsBest({listings}) {
+    return (
+    <div className="world-best">
+      <div className="world-cards">
+        <div style={{display: "flex"}}>
+        <Link to={`/listings/${listings[1]?.id}`} className="world-cards1">
+            <div className="world-photo">
+              <img  src={listings[1]?.photoUrls[0]}/>
+            </div>
+            <p className="world-car-p1">{listings[1]?.property_name}</p>
+            <p className="world-car-p2">{listings[1]?.city}, {listings[1]?.country}</p>
+        </Link>
+        <Link to={`/listings/${listings[2]?.id}`} className="world-cards2">
+            <div className="world-photo">
+              <img  src={listings[2]?.photoUrls[0]}/>
+            </div>
+            <p className="world-car-p1">{listings[2]?.property_name}</p>
+            <p className="world-car-p2">{listings[2]?.city}, {listings[2]?.country}</p>
+        </Link>
+        </div>
+
+        <div style={{display: "flex"}}>
+          <div className="world-text" style={{width: "30%"}}>
+            <h3>
+              The world's best hostels, with over
+              <span style={{color: "#30cf72", marginLeft: "10px"}}>13 million reviews</span>
+              .
+            </h3>
+            <p>With over 16,500 hostels in 180 countries, there’s always room for a new adventure!</p>
+          </div>
+          <Link to={`/listings/${listings[6]?.id}`} className="world-cards3">
+            <div className="world-photo">
+              <img  src={listings[6]?.photoUrls[0]}/>
+            </div>
+            <p className="world-car-p1">{listings[6]?.property_name}</p>
+            <p className="world-car-p2">{listings[6]?.city}, {listings[6]?.country}</p>
+        </Link>
+        </div>
+
+        <div style={{display: "flex"}}>
+        <Link to={`/listings/${listings[4]?.id}`} className="world-cards4">
+            <div className="world-photo">
+              <img  src={listings[4]?.photoUrls[0]}/>
+            </div>
+            <p className="world-car-p1">{listings[4]?.property_name}</p>
+            <p className="world-car-p2">{listings[4]?.city}, {listings[4]?.country}</p>
+        </Link>
+        <Link to={`/listings/${listings[5]?.id}`} className="world-cards5">
+            <div className="world-photo">
+              <img  src={listings[5]?.photoUrls[0]}/>
+            </div>
+            <p className="world-car-p1">{listings[5]?.property_name}</p>
+            <p className="world-car-p2">{listings[5]?.city}, {listings[5]?.country}</p>
+        </Link>
+        </div>
+      </div>
+    </div>
+    )
+}
--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -28,9 +28,9 @@ import beach from "../../assets/pictures/palmar.beach.lodge.jpg"
 import eco from "../../assets/pictures/icons/eco.svg"
 import cnc from "../../assets/pictures/icons/cnc.svg"
 import InstaPage from "./Instapage";
+import WorldsBest from "./WorldsBest";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchListings } from "../../store/listings";
-import { Link } from 'react-router-dom';
 
 
 export default function HomePage() {
@@ -195,81 +195,28 @@ export default function HomePage() {
       </div>
     </div>
 
-    <div className="world-best">
-      <div className="world-cards">
-        <div style={{display: "flex"}}>
-        <Link to={`/listings/${listings[1]?.id}`} className="world-cards1">
-            <div className="world-photo">
-              <img  src={listings[1]?.photoUrls[0]}/>
-            </div>
-            <p className="world-car-p1">{listings[1]?.property_name}</p>
-            <p className="world-car-p2">{listings[1]?.city}, {listings[1]?.country}</p>
-        </Link>
-        <Link to={`/listings/${listings[2]?.id}`} className="world-cards2">
-            <div className="world-photo">
-              <img  src={listings[2]?.photoUrls[0]}/>
-            </div>
-            <p className="world-car-p1">{listings[2]?.property_name}</p>
-            <p className="world-car-p2">{listings[2]?.city}, {listings[2]?.country}</p>
-        </Link>
-        </div>
+    <WorldsBest listings={listings}/>
 
-        <div style={{display: "flex"}}>
-          <div className="world-text" style={{width: "30%"}}>
-            <h3>
-              The world's best hostels, with over
-              <span style={{color: "#30cf72", marginLeft: "10px"}}>13 million reviews</span>
-              .
-            </h3>
-            <p>With over 16,500 hostels in 180 countries, there’s always room for a new adventure!</p>
-          </div>
-          <Link to={`/listings/${listings[6]?.id}`} className="world-cards3">
-            <div className="world-photo">
-              <img  src={listings[6]?.photoUrls[0]}/>
-            </div>
-            <p className="world-car-p1">{listings[6]?.property_name}</p>
-            <p className="world-car-p2">{listings[6]?.city}, {listings[6]?.country}</p>
-        </Link>
-        </div>
-
-        <div style={{display: "flex"}}>
-        <Link to={`/listings/${listings[4]?.id}`} className="world-cards4">
-            <div className="world-photo">
-              <img  src={listings[4]?.photoUrls[0]}/>
-            </div>
-            <p className="world-car-p1">{listings[4]?.property_name}</p>
-            <p className="world-car-p2">{listings[4]?.city}, {listings[4]?.country}</p>
-        </Link>
-        <Link to={`/listings/${listings[5]?.id}`} className="world-cards5">
-            <div className="world-photo">
-              <img  src={listings[5]?.photoUrls[0]}/>
-            </div>
-            <p className="world-car-p1">{listings[5]?.property_name}</p>
-            <p className="world-car-p2">{listings[5]?.city}, {listings[5]?.country}</p>
-        </Link>
-        </div>
+    <div className="commited-world">
+      <div className="world-pic">
+        <img src={beach}/>
+        <p style={{marginTop: "2rem"}}>Palmar Beach Lodge, Panama</p>
+        <p style={{fontWeight: "400"}}>Hoscars 2022 Winner</p>
       </div>
-
-      <div className="commited-world">
-        <div className="world-pic">
-          <img src={beach}/>
-          <p style={{marginTop: "2rem"}}>Palmar Beach Lodge, Panama</p>
-          <p style={{fontWeight: "400"}}>Hoscars 2022 Winner</p>
-        </div>
-        <div>
-          <div className="world-text">
-            <img src={eco}/>
-            <h2>
-            We are commited to a 
-            <span style={{color: "#30cf72", marginLeft: "10px"}}>better world</span>
-            .
-            </h2>
-            <p>Join us in making the planet a priority. We are proud to be a Climate Neutral verified company, committed to reducing our environmental footprint.</p>
-            <img src={cnc}/>
-          </div>
+      <div>
+        <div className="world-text">
+          <img src={eco}/>
+          <h2>
+          We are commited to a 
+          <span style={{color: "#30cf72", marginLeft: "10px"}}>better world</span>
+          .
+          </h2>
+          <p>Join us in making the planet a priority. We are proud to be a Climate Neutral verified company, committed to reducing our environmental footprint.</p>
+          <img src={cnc}/>
         </div>
       </div>
     </div>
+    
     <InstaPage/>
     <Footer/>
     </>
