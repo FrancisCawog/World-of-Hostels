a7b6a85a5634566fd67998e0103735e09e9a0829
Francis Cawog <Franciscawog@gmail.com>
2024-10-25 17:12:58 -0400

    reorganized sign up form

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -12,13 +12,15 @@ import { FaEye, FaEyeSlash } from 'react-icons/fa';
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+
   const [email, setEmail] = useState("");
   const [firstName, setFirstName] = useState("");
-  const [dateOfBirth, setDateOfBirth] = useState("");
   const [lastName, setLastName] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
   const [password, setPassword] = useState("");
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [errors, setErrors] = useState([]);
+  
   const [passwordConditions, setPasswordConditions] = useState({
     hasLowercase: false,
     hasUppercase: false,
@@ -42,6 +44,10 @@ function SignupFormPage() {
     setValidPassword((trueConditionsCount >= 3 && hasEightCharacters) || password.length === 0);
   }, [password]);
 
+  const handleTogglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
@@ -59,28 +65,23 @@ function SignupFormPage() {
       });
   };
 
-  const handleTogglePasswordVisibility = () => {
-    setIsPasswordVisible(!isPasswordVisible);
-  };
-
-  function formatInputDate(inputDate) {
+  const formatInputDate = (inputDate) => {
     const cleanedInput = inputDate.replace(/\D/g, '');
     if (cleanedInput.length === 8) {
-        const year = cleanedInput.slice(0,4);
-        const month = cleanedInput.slice(4, 6);
-        const day = cleanedInput.slice(-2);
-        return `${year}-${month}-${day}`;
+      const year = cleanedInput.slice(0, 4);
+      const month = cleanedInput.slice(4, 6);
+      const day = cleanedInput.slice(-2);
+      return `${year}-${month}-${day}`;
     }
     return inputDate;
-}
+  };
 
-function isValidAge(dateOfBirth) {
-  if (dateOfBirth) {
+  const isValidAge = (dateOfBirth) => {
+    if (dateOfBirth) {
       const cleanedInput = dateOfBirth.replace(/\D/g, '');
       if (cleanedInput.length !== 8) {
-          return false;
+        return false;
       }
-
       const [year, month, day] = dateOfBirth.split('-');
       const dayNum = parseInt(day, 10);
       const monthNum = parseInt(month, 10);
@@ -95,43 +96,33 @@ function isValidAge(dateOfBirth) {
         return false;
       }
 
-      const today2 = new Date();
-      let age = today2.getFullYear() - dob.getFullYear();
-      const monthDiff = today2.getMonth() - dob.getMonth();
+      const today = new Date();
+      let age = today.getFullYear() - dob.getFullYear();
+      const monthDiff = today.getMonth() - dob.getMonth();
 
-      if (monthDiff < 0 || (monthDiff === 0 && today2.getDate() < dob.getDate())) {
-          age--;
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+        age--;
       }
 
       return age >= 18 && age <= 100;
-  } else {
-      return false; 
-  }
-}
+    } 
+    return false; 
+  };
 
-function isValidLength(password){
-  if (password.length < 8){
-    return false
-  } else {
-    return true
-  }
-}
+  const isValidLength = (password) => {
+    return password.length >= 8;
+  };
 
-function isValidEmail(email){
-  const atIndex = email.indexOf('@')
-  const dotIndex = email.indexOf('.')
+  const isValidEmail = (email) => {
+    const atIndex = email.indexOf('@');
+    const dotIndex = email.indexOf('.');
+    const atSym = email.split('@').length === 2 && atIndex !== 0;
+    const dotSym = email.split('.').length === 2 && dotIndex > atIndex + 1;
 
-  const atSym = email.split('@').length === 2 && atIndex !== 0;
-  const dotSym = email.split('.').length === 2 && dotIndex > atIndex + 1;
+    return atSym && dotSym && dotIndex <= email.length - 3;
+  };
 
-  if (atSym && dotSym && dotIndex <= email.length - 3) {
-    return true
-  } else {
-    return false
-  }
-}
-
-if (sessionUser) return <Redirect to="/" />;
+  if (sessionUser) return <Redirect to="/" />;
 
   return (
     <>
@@ -139,30 +130,32 @@ if (sessionUser) return <Redirect to="/" />;
         <div className="signupContainer">
           <div className="signupBox">
             <Link to="/" exact="true" style={{ textDecoration: 'underline', color: 'black', display: 'flex', alignItems: 'center', fontFamily: "Inter" }}>
-              <img src={MyArrowSVG} alt="Back" style={{ width: '14px', marginTop: "2px" }}/> Back
+              <img src={MyArrowSVG} alt="Back" style={{ width: '14px', marginTop: "2px" }} /> Back
             </Link>
             <br />
-            <img src={logo} style={{ width: '50px' }}/>
+            <img src={logo} style={{ width: '50px' }} />
             <h1>Sign up to add more adventure!</h1>
-            <p className="log-in-p" style={{fontSize: "12px"}}> Sign up to agree to the use of your information. To learn more, visit our social terms of service and privacy policy pages. </p>
+            <p className="log-in-p" style={{ fontSize: "12px" }}>
+              Sign up to agree to the use of your information. To learn more, visit our social terms of service and privacy policy pages.
+            </p>
             <form onSubmit={handleSubmit}>
-            <ul>
-              {errors.map((error) => 
-                error === "Email has already been taken" ? (
-                  <div key="email-error" className="email-error">
-                    <img src="https://a.hwstatic.com/image/upload/hw/auth0/warning.svg" alt="Warning" />
-                    <div className="error-message">
-                      <span>You already have an account, try login instead!</span>
+              <ul>
+                {errors.map((error) =>
+                  error === "Email has already been taken" ? (
+                    <div key="email-error" className="email-error">
+                      <img src="https://a.hwstatic.com/image/upload/hw/auth0/warning.svg" alt="Warning" />
+                      <div className="error-message">
+                        <span>You already have an account, try login instead!</span>
+                      </div>
+                      <div className="error-login">
+                        <Link id="bottom" className="link" to="/login" style={{ fontFamily: "Inter-bold" }}>Login</Link>
+                      </div>
                     </div>
-                    <div className="error-login">
-                      <Link id="bottom" className="link" to="/login" style={{fontFamily: "Inter-bold"}}>Login</Link>
-                    </div>
-                  </div>
-                ) : (
-                  <li key={error}>{error}</li>
-                )
-              )}
-            </ul>
+                  ) : (
+                    <li key={error}>{error}</li>
+                  )
+                )}
+              </ul>
 
               <div className={`input-wrapper-login ${firstName ? 'non-empty' : ''}`}>
                 <input
@@ -225,32 +218,32 @@ if (sessionUser) return <Redirect to="/" />;
                   {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
                 </button>
 
-                {!validPassword &&
+                {!validPassword && (
                   <div className="password-check">
                     <span style={{ color: password.length >= 8 ? 'green' : 'red' }}>
                       At least 8 characters
                     </span>
                     <span>Contain at least 3 of the following 4 types of characters:</span>
                     <span style={{ color: passwordConditions.hasLowercase ? 'green' : 'white' }}>
-                      Lower case letters (a-z)
+                      {passwordConditions.hasLowercase ? '✓' : '✗'} Lowercase Letters
                     </span>
                     <span style={{ color: passwordConditions.hasUppercase ? 'green' : 'white' }}>
-                      Upper case letters (A-Z)
+                      {passwordConditions.hasUppercase ? '✓' : '✗'} Uppercase Letters
                     </span>
                     <span style={{ color: passwordConditions.hasNumber ? 'green' : 'white' }}>
-                      Numbers (i.e. 0-9)
+                      {passwordConditions.hasNumber ? '✓' : '✗'} Numbers
                     </span>
                     <span style={{ color: passwordConditions.hasSpecialChar ? 'green' : 'white' }}>
-                      Special characters (e.g. !@#$%^&*)
+                      {passwordConditions.hasSpecialChar ? '✓' : '✗'} Special Characters
                     </span>
                   </div>
-                }
+                )}
               </div>
 
               <button
                 id="loginButton"
-                type="submit"
                 className="signup-button"
+                type="submit"
                 style={{ 
                   pointerEvents: (isValidEmail(email) && isValidLength(password) && firstName && lastName && isValidAge(dateOfBirth) && validPassword) ? 'auto' : 'none',
                   opacity: (isValidEmail(email) && isValidLength(password) && firstName && lastName && isValidAge(dateOfBirth) && validPassword) ? 1 : 0.5
@@ -262,9 +255,8 @@ if (sessionUser) return <Redirect to="/" />;
 
             <br />
             <p id="bottom">Already have an account?</p>
-            <Link id="bottom" className="link" to="/login" style= {{fontFamily: "Inter-bold"}}>
-              <img src={UserSVG} alt="Back" style={{ width: '14px' }}/>Login
-            </Link>
+
+            <Link id="bottom" className="link" to="/login" style= {{fontFamily: "Inter-bold"}}> <img src={UserSVG} alt="Back" style={{ width: '14px' }}/>Login </Link>
           </div>
         </div>
 
